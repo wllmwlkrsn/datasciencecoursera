7842895243c288dc9198be35baa8ea38e1aa8b28
wllmwlkrsn <wllm.wkrsn@gmail.com>
2020-04-12 22:05:43 -0400

    ?

---
+++ R - Coursera/my_first_R_script.R
@@ -0,0 +1,6 @@
+1+1
+example <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8), nrow = 4, ncol = 2)
+View(example)
+View(example)
+source('~/.active-rstudio-document')
+savehistory("~/R - Coursera/my_first_R_script.R")
